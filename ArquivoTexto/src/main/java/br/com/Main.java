0f515cc069b769385d245b18f6ac3c72b50e58ec
rafael.stuzata <rafael.stuzata@flowanalytics.com.br>
2026-08-30 19:58:29 -0300

    arquivo de texto exercio

--- ArquivoTexto/src/main/java/br/com/Main.java
+++ ArquivoTexto/src/main/java/br/com/Main.java
@@ -4,34 +4,51 @@ import java.io.*;
 
 public class Main {
     static void main() {
-        String nomeArq = "arquivo_texto.txt";
+        String a1 = "arq1.txt";
+        String a2 = "arq2.txt";
 
-        File file = new File(nomeArq);
+        File f1 = new File(a1);
+        File f2 = new File(a2);
 
+        StringBuilder sb = new StringBuilder();
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-            writer.write("DESISTA DOS SEUS SONHOS E MORRA");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(f1))) {
+            for (int i = 0; i <= 10; i++) {
+                writer.write("5 * " + i + " = " + 5 * i + "\n");
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        if (file.exists()) {
-            System.out.printf(file.getAbsolutePath());
+        if (f1.exists()) {
+            System.out.println(f1.getAbsolutePath());
 
-            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                int character;
-                while ((character = reader.read()) != -1) {
-                    System.out.println((char) character);
-                }
+            try (BufferedReader reader = new BufferedReader(new FileReader(f1))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
                     System.out.println(line);
+                    sb.append(line).append("\n");
                 }
             } catch (IOException e) {
                 System.out.println("erro ao ler o arquivo");
             }
         } else {
-            System.out.println("NAO EXISTE O ARQUIVO SR FAROFA");
+            System.out.println("NAO EXISTE O ARQUIVO");
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(f2))) {
+            writer.write(sb.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(f2))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("erro ao ler o arquivo");
         }
     }
 }
